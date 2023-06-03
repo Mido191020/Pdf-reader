2defaefe2327a4a4e301c1466c716a7eb91aad24
mido <at744245@gmail.com>
2023-06-03 21:25:17 +0300

    everything is ok

--- main.py
+++ main.py
@@ -12,42 +12,49 @@ def play_pdf():
     pdfreader = PdfReader(book)
     pages = len(pdfreader.pages)
 
-    for num in range(pages):
-        page = pdfreader.pages[num]
-        text = page.extract_text()
+    root = tk.Tk()
+    text_widget = tk.Text(root, height=20, width=70)
+    text_widget.pack()
 
-        root = tk.Tk()
-        text_widget = tk.Text(root, height=20, width=50)
-        text_widget.pack()
+    def speak():
+        global player  # Access the global player variable
+        player = pyttsx3.init()
+        player.setProperty('rate', 150)  # Adjust the speech rate (change the value as desired)
+        voices = player.getProperty('voices')
+        player.setProperty('voice', voices[0].id)  # Select the first available voice (change index as desired)
 
-        def speak():
-            global player  # Access the global player variable
-            player = pyttsx3.init()
-            player.setProperty('rate', 150)  # Adjust the speech rate (change the value as desired)
-            voices = player.getProperty('voices')
-            player.setProperty('voice', voices[0].id)  # Select the first available voice (change index as desired)
-
+        for num in range(pages):
+            page = pdfreader.pages[num]
+            text = page.extract_text()
             player.say(text)
             player.runAndWait()
 
-        def stop():
-            global player  # Access the global player variable
-            if player is not None:
-                player.stop()
+    def stop():
+        global player  # Access the global player variable
+        if player is not None:
+            player.stop()
+        root.quit()  # Quit the Tkinter application
 
-        # Add a colored welcome message
-        welcome_message = "Welcome to the PDF Audiobook Player!\n\nThis is Mido Tarek."
-        text_widget.tag_configure("welcome", foreground="blue")  # Set the color of the tag
+    # Add a colored welcome message
+    welcome_message = "Welcome to the PDF Audiobook Player!\n\nThis application allows you to turn PDF files into audio books.\n\nSit back, relax, and enjoy listening to your favorite books.\n\nDeveloped by Mido Tarek."
 
-        # Display the welcome message
-        text_widget.insert(tk.END, welcome_message, "welcome")  # Apply the tag to the welcome message
+    # Customizing the font settings
+    font_family = "Arial"
+    font_size = 16
+    font_weight = "bold"
+    font_style = "italic"
 
-        btn_speak = ttk.Button(root, text="Speak", command=speak)
-        btn_stop = ttk.Button(root, text="Stop", command=stop)
+    # Applying the formatted text and font settings
+    text_widget.tag_configure("welcome", foreground="blue", font=(font_family, font_size, font_weight, font_style))
+    text_widget.insert(tk.END, welcome_message, "welcome")
 
-        btn_speak.pack()
-        btn_stop.pack()
+    btn_speak = ttk.Button(root, text="Speak", command=speak)
+    btn_stop = ttk.Button(root, text="Exit", command=stop)
 
-        root.mainloop()
+    btn_speak.pack()
+    btn_stop.pack()
+
+    root.protocol("WM_DELETE_WINDOW", stop)  # Handle window close event
+    root.mainloop()
 
 play_pdf()
